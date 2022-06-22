af41325a1eb78a6aefe5a135ab50e17ce44736c3
Nora Himed <norahimed1@gmail.com>
2022-06-22 10:04:46 -0400

    playRound initates one game of R, P, S

--- javascript.js
+++ javascript.js
@@ -8,3 +8,40 @@ function computerPlay() {
     else 
         return 'Scissors';
 }
+
+function playRound(playerSelection, computerSelection) {
+    pS = playerSelection.toUpperCase();
+    cS = computerSelection.toUpperCase();
+
+    if (pS == cS)
+        return "It's a tie";
+    if (pS == 'ROCK')
+    {
+        if (cS == 'SCISSORS')
+            return "You Win! Rock beats Scissors.";
+        else
+            return "You Lose! Rock beats Scissors.";
+    }
+
+    if (pS == 'PAPER')
+    {
+        if (cS == 'ROCK')
+            return "You Win! Paper beats Rock.";
+        else
+            return "You Lose! Paper beats Rock.";
+    }
+
+    if (pS == 'SCISSORS')
+    {
+        if (cS == 'PAPER')
+            return "You Win! Scissors beats Paper.";
+        else
+            return "You Lose! Scissors beats Paper.";    
+    }
+
+
+}
+
+const playerSelection = "Rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection))
